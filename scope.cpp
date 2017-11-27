3f2aab47f416493dcb3475471dd68e725ecbf861
Willard <willard.torres1008@gmail.com>
2017-11-27 11:43:19 +0800

    Add more flexible casting between Int and Float

--- scope.cpp
+++ scope.cpp
@@ -11,12 +11,22 @@ namespace Prototype {
 
     void Scope::declare(string name, string type, Expr_ptr value) {
         types[name] = type;
-        variables[name] = (value != NULL? value : null_expr);
+        assign(name, (value != NULL? value : null_expr));
     }
 
     bool Scope::assign(string name, Expr_ptr value) {
         if(types.find(name) == types.end()) {
             return false;
+        }
+        string value_type = type_to_name(value->type);
+
+        if(types[name] == "float" && value_type == "int") {
+            variables[name] = make_shared<Float>(static_pointer_cast<Int>(value)->value);
+            return true;
+        }
+        if(types[name] == "int" && value_type == "float") {
+            variables[name] = make_shared<Int>(int(static_pointer_cast<Float>(value)->value));
+            return true;
         }
         if(types[name] != "var" && types[name] != type_to_name(value->type)) {
             logger->log(value, "ERROR", "Cannot assign value of type " + type_to_name(value->type) + " to variable of type " + types[name]);
